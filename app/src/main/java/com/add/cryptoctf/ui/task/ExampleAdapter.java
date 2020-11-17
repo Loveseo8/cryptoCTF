177ab048f0a3121a519c39a4c750c2e444e0512c
Loveseo8 <58880849+Loveseo8@users.noreply.github.com>
2020-11-17 22:53:44 +0300

    -read

--- app/src/main/java/com/add/cryptoctf/ui/task/ExampleAdapter.java
+++ app/src/main/java/com/add/cryptoctf/ui/task/ExampleAdapter.java
@@ -1,9 +1,12 @@
 package com.add.cryptoctf.ui.task;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -13,17 +16,38 @@ import java.util.List;
 
 public class ExampleAdapter extends RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder> {
 
-private List<Task> tasks;
+    private List<Task> tasks;
+    private OnItemClickListener mListener;
 
-public static class ExampleViewHolder extends RecyclerView.ViewHolder {
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        mListener = listener;
+    }
+
+    public static class ExampleViewHolder extends RecyclerView.ViewHolder {
 
     public TextView titlel;
     public TextView pointss;
 
-    public ExampleViewHolder(View itemView) {
+    public ExampleViewHolder(View itemView, final OnItemClickListener listener) {
         super(itemView);
         titlel = (TextView) itemView.findViewById(R.id.title);
         pointss = (TextView) itemView.findViewById(R.id.points);
+
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+                        listener.onItemClick(position);
+                    }
+                }
+            }
+        });
     }
 }
     public ExampleAdapter(List<Task> tasks) {
@@ -32,7 +56,7 @@ public static class ExampleViewHolder extends RecyclerView.ViewHolder {
     @Override
     public ExampleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_item, parent, false);
-        ExampleViewHolder evh = new ExampleViewHolder(v);
+        ExampleViewHolder evh = new ExampleViewHolder(v, mListener);
         return evh;
     }
     @Override
--- app/src/main/java/com/add/cryptoctf/ui/task/TaskFragment.java
+++ app/src/main/java/com/add/cryptoctf/ui/task/TaskFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,16 +13,23 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.add.cryptoctf.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TaskFragment extends Fragment {
 
-    private List<Task> tasks;
+    private ArrayList<Task> tasks = new ArrayList<>();
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private DatabaseReference tasksReference;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -33,24 +41,13 @@ public class TaskFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        tasks = new ArrayList<>();
-        tasks.add(new Task("Line 1", "63"));
-        tasks.add(new Task("Line 1", "63"));
-        tasks.add(new Task("Line 1", "63"));
-        tasks.add(new Task("Line 1", "63"));
-        tasks.add(new Task("Line 1", "63"));
-        tasks.add(new Task("Line 1", "63"));
-        tasks.add(new Task("Line 1", "63"));
-        tasks.add(new Task("Line 1", "63"));
-        tasks.add(new Task("Line 1", "63"));
-        tasks.add(new Task("Line 1", "63"));
-        tasks.add(new Task("Line 1", "63"));
-        tasks.add(new Task("Line 1", "63"));
-        tasks.add(new Task("Line 1", "63"));
+        init();
 
+        recycler(view);
 
+    }
 
-
+    public void recycler(View view){
 
         mRecyclerView = view.findViewById(R.id.rv);
         mRecyclerView.setHasFixedSize(true);
@@ -58,8 +55,39 @@ public class TaskFragment extends Fragment {
         mAdapter = new ExampleAdapter(tasks);
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
+
     }
+
+    public void init(){
+
+        tasksReference = FirebaseDatabase.getInstance().getReference("Tasks");
+
+        tasks.clear();
+
+        tasksReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot){
+
+            showData(snapshot);
+        }
+
+        public void showData(DataSnapshot dataSnapshot){
+
+            for (DataSnapshot ds : dataSnapshot.getChildren()) {
+
+                Task task = new Task(ds.child("title").getValue().toString(), ds.child("points").getValue().toString());
+                tasks.add(task);
+            }
+
+        }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
+}
 
 class Task {
     String title;
@@ -67,6 +95,22 @@ class Task {
 
     Task(String title, String points) {
         this.title = title;
+        this.points = points;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPoints() {
+        return points;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPoints(String points) {
         this.points = points;
     }
 }
